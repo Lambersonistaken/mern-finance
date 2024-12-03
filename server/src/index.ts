278827158df4a94ee69b289c2eb3d86f4d09b735
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-03 19:27:16 +0300

    db connection is ongoing

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Express } from "express"
+import mongoose from "mongoose"
+
+
+const app: Express = express();
+
+const port = process.env.PORT || 3001
+
+
+app.use(express.json());
+
+const mongoURI: string =
