1c8a573a48c4d795f8a3205755b0df85683fb78f
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-03 05:09:18 +0300

    auth dashboard route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import "./App.css"
+import Auth from "./pages/auth";
+import Dashboard from "./pages/dashboard";
 
 export default function App() {
   return (
         <Router>  
           <div className="app-container">
             <Routes>
-              <Route path="/" element={<h1>Dashboard</h1>}></Route>
-              <Route path="/auth" element={<h1>Sign In</h1>}></Route>
+              <Route path="/" element={<Dashboard/>}></Route>
+              <Route path="/auth" element={<Auth/>}></Route>
             </Routes>
           </div>
         </Router>
