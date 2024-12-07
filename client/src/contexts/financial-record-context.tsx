9fbca7a1bf171fcbb3f4775b65891e740dfa5efc
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-07 18:35:41 +0300

    context creating

---
+++ client/src/contexts/financial-record-context.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-refresh/only-export-components */
+import { createContext, useState } from "react";
+
+
+interface FinancialRecord {
+    id?: string;
+    userId: string;
+    date: Date;
+    description: string;
+    amount: number;
+    category: string;
+    paymentMethod: string;
+}
+
+interface FinancialRecordContextType {
+    records: FinancialRecord[];
+    addRecord: (record: FinancialRecord) => void;
+    // updateRecord: (id: string, newRecord: FinancialRecord) => void;
+    // deleteRecord: (id: string) => void;
+}
+
+
+
+
+export const FinancialRecordsContext = createContext<FinancialRecordContextType | undefined>(undefined)
+
+export const FinancialRecordsProvider = ({children,} : {children: React.ReactNode;}) => {
+    
+    const [records, setRecords] = useState<FinancialRecord[]>([]);
+
+    const addRecord = (record: FinancialRecord) => {}
+
+    return (
+        <FinancialRecordsContext.Provider value={{records, addRecord}}>
+            {children}
+        </FinancialRecordsContext.Provider>
+    )
+
+}
